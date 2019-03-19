cea695e93217a49b81b5361cdd03e126a060d097
yc <yangchaocba@163.com>
2019-03-19 15:24:17 +0800

    注释

--- hooliganstepone/app/src/main/java/yc/com/hooliganstepone/MainActivity.java
+++ hooliganstepone/app/src/main/java/yc/com/hooliganstepone/MainActivity.java
@@ -9,6 +9,12 @@ import yc.com.hooliganstepone.service.BrightService;
 import yc.com.hooliganstepone.service.MyService;
 import yc.com.hooliganstepone.service.RemoteService;
 
+/**
+ * 这两个保活主要就是通过提高应用的adj来实现的
+ * 如果想查看自己应用的adj可以通过
+ * cat /proc/进程id（这个可以在as上看也可以通过ps命令来看）/oom_adj
+ * 记住了cat后面有一个空格啊
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
